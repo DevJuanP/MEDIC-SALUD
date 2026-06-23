fa607a0c20f3b9a5141bdf13a1a985310ec7cab2
DevJuanP <juanpajueloch@gmail.com>
2026-06-22 23:36:28 -0500

    times

--- app/src/main/java/pe/edu/cibertec/medic_salud/network/RetrofitClient.kt
+++ app/src/main/java/pe/edu/cibertec/medic_salud/network/RetrofitClient.kt
@@ -1,21 +1,35 @@
 package pe.edu.cibertec.medic_salud.network
 
+import okhttp3.OkHttpClient
 import pe.edu.cibertec.medic_salud.data.remote.api.PatientAuthApi
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
 
     private const val BASE_URL = "https://api.rxlink.xyz/"
 
-    private val retrofit: Retrofit by lazy {
+    private var okHttpClient = OkHttpClient.Builder()
+        .connectTimeout(1, TimeUnit.MINUTES)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .writeTimeout(30, TimeUnit.SECONDS)
+        .build()
+
+    private fun buildRetrofit() = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .client(okHttpClient)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    /*private val retrofit: Retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-    }
+    }*/
 
     val patientAuthApi: PatientAuthApi by lazy {
-        retrofit.create(PatientAuthApi::class.java)
+        buildRetrofit().create(PatientAuthApi::class.java)
     }
 }
